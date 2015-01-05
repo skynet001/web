a3f099d49cd2eee1d960bb147ac003bce5f13001
letao99 <letao99@qq.com>
2015-01-05 16:23:18 +0800

    新增用户实体

---
+++ src/com/skynet/enetity/User.java
@@ -0,0 +1,42 @@
+package com.skynet.enetity;
+
+public class User
+{
+    
+    private String userName;
+    
+    private String sex;
+    
+    private Integer age;
+    
+    public String getUserName()
+    {
+        return userName;
+    }
+    
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+    
+    public String getSex()
+    {
+        return sex;
+    }
+    
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+    
+    public Integer getAge()
+    {
+        return age;
+    }
+    
+    public void setAge(Integer age)
+    {
+        this.age = age;
+    }
+    
+}
